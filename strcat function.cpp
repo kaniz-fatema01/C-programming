40c01fd309b33cbfbef3862b5b94f12bae0c543b
Kaniz Fatema <78794903+kaniz-fatema01@users.noreply.github.com>
2021-02-26 17:17:06 +0600

    Create strcat function.cpp

---
+++ strcat function.cpp
@@ -0,0 +1,23 @@
+include<stdio.h>
+#include<string.h>
+
+int main()
+{
+   int i;
+   int count1=0;
+    char str1[1000],str2[1000];
+
+    gets(str1);
+    gets(str2);
+
+    strcat(str2,str1);
+
+
+    printf("%s  ",str2);
+
+
+return 0;
+
+
+
+}
