2cfb3c318bad55e59a7e1ce33e477bbfd01b574a
Kaniz Fatema <78794903+kaniz-fatema01@users.noreply.github.com>
2021-02-26 18:31:00 +0600

    Create abs function.cpp

---
+++ abs function.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+   int b = -10,a=0;
+   int count1=0;
+   float result=0;
+    char str1[1000],str2[1000];
+
+    scanf("%d",&a);
+    //scanf("%d",&b);
+
+    result = sqrt(a);
+
+    printf("%d",abs(a));
+
+
+
+
+return 0;
+
+
+
+}
