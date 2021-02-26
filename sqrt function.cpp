47ef24826c53e39de6f9d91b89fa50e773ecbe87
Kaniz Fatema <78794903+kaniz-fatema01@users.noreply.github.com>
2021-02-26 18:16:04 +0600

    Create sqrt function.cpp

---
+++ sqrt function.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+   int b = 0,a=0;
+   int count1=0;
+   float result=0;
+    char str1[1000],str2[1000];
+
+    scanf("%d",&a);
+   
+
+    result = sqrt(a);
+
+    printf("%f",result);
+
+
+
+return 0;
+
+
+
+}
