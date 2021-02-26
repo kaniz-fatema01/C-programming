aee1ae298d642f3fca04d65a4f92af26472644ee
Kaniz Fatema <78794903+kaniz-fatema01@users.noreply.github.com>
2021-02-26 18:27:53 +0600

    Create pow_function.cpp

---
+++ pow_function.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+   int b = 0,a=0;
+   int count1=0,result = 0;
+    char str1[1000],str2[1000];
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    result = pow(a,b);
+
+    printf("%d",result);
+
+
+
+return 0;
+
+
+
+}
